23438894c2184e44ee6df1bda8e74aa8cecfa353
Edward Tanguay (hm.win) <edward@tanguay.info>
2021-12-08 00:59:43 +0100

    works

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,12 +1,12 @@
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 const Nav = () => {
 	return (
 		<nav>
 			<ul>
-				<li><Link to="/">Welcome</Link></li>
-				<li><Link to="register">Register</Link></li>
-				<li><Link to="login">Login</Link></li>
+				<li><NavLink to="/">Welcome</NavLink></li>
+				<li><NavLink to="register">Register</NavLink></li>
+				<li><NavLink to="login">Login</NavLink></li>
 			</ul>
 		</nav>
 	)
